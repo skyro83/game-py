638fe6f59edb99a7be3bb834b2f3db437df22961
skyro83 <cardinale.dorian83200@gmail.com>
2024-05-20 21:13:40 +0200

    Update
    Update

---
+++ level/level.py
@@ -0,0 +1,63 @@
+import random
+from tkinter import *
+
+def first_level(root, canvas, square, signe_x, signe_y, vitesse):
+    target_x = random.randint(10, 500)
+    target_y = random.randint(5, 300)
+    target = canvas.create_rectangle(target_x, target_y, target_x + 100, target_y + 100, fill="blue")
+
+    while True:
+        canvas.move(square, vitesse * signe_x[0], vitesse * signe_y[0])
+        canvas.update()
+        if canvas.coords(square)[2] >= 900 or canvas.coords(square)[0] <= 0:
+            signe_x[0] *= -1
+        if canvas.coords(square)[3] >= 800 or canvas.coords(square)[1] <= 0:
+            signe_y[0] *= -1
+        if (canvas.coords(square)[0] <= target_x + 10 and canvas.coords(square)[2] >= target_x + 90) and (canvas.coords(square)[1] <= target_y + 10 and canvas.coords(square)[3] >= target_y + 90):
+            print("Level completed!")
+            canvas.delete(target)
+            second_level(root, canvas, square, signe_x, signe_y, vitesse)
+        canvas.after(1)
+        canvas.update()
+
+
+def second_level(root, canvas, square, signe_x, signe_y, vitesse):
+    y_milieu=(canvas.coords(square)[1]+canvas.coords(square)[3])/2
+    block1 = canvas.create_rectangle(50, y_milieu -100, 25, y_milieu+100, fill="black")
+
+    canvas.update()
+    existe=False
+    Existe=False
+    while True:
+
+        if canvas.coords(block1)[1] + vitesse * signe_y[0]<0:
+            if not(Existe):
+                N=canvas.create_rectangle(50, -100, 25, 200, fill="black")
+                Existe=True
+        elif Existe:
+                canvas.delete(N)
+                Existe = False
+
+        elif canvas.coords(block1)[3] + vitesse * signe_y[0]>800:
+            if not(existe):
+                n=canvas.create_rectangle(50, 600, 25, 800, fill="black")
+                existe=True
+        else:
+            if existe:
+                canvas.delete(n)
+                existe = False
+        
+        canvas.move(square, vitesse * signe_x[0], vitesse * signe_y[0])
+        
+
+        canvas.move(block1, 0, vitesse * signe_y[0]  )
+        canvas.update()
+        if canvas.coords(square)[2] >= 900 or canvas.coords(square)[0] <= 0:
+            signe_x[0] *= -1
+        if canvas.coords(square)[3] >= 800 or canvas.coords(square)[1] <= 0:
+            signe_y[0] *= -1
+
+        if canvas.coords(square)[0] <= 50:
+            signe_x[0] *= -1
+        canvas.after(1)
+        canvas.update()
